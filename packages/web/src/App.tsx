4e62b1340331e4d06f8184de0a84aa8937e12db4
0xpolarzero <0xpolarzero@gmail.com>
2025-03-09 18:19:13 +0100

    init: vite + shadcn

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+import "@/App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh bg-background dark">
+      <Button className="cursor-pointer">Click me</Button>
+    </div>
+  );
+}
+
+export default App;
